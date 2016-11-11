5c1718060c42c017b4dd04ae4416b32e557fdb63
sammobi <sam.mobi@simpalm.com>
2016-11-11 15:45:24 -0500

    working on location provider

--- app/src/main/java/com/simpalm/meetingtracker/CreateEvent.java
+++ app/src/main/java/com/simpalm/meetingtracker/CreateEvent.java
@@ -4,10 +4,8 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Address;
-import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -21,6 +19,13 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.kunzisoft.switchdatetime.SwitchDateTimeDialogFragment;
 
 import java.io.IOException;
@@ -29,79 +34,92 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class CreateEvent extends BaseLocation {
+public class CreateEvent extends BaseLocation implements PlaceSelectionListener {
 
     private Toolbar mToolbar;
-    private EditText mEventTitleEt, mEventDescriptionEt, mEventLocationEt;
+    private EditText mEventTitleEt, mEventDescriptionEt;
     private Button mCreateEventBtn;
+    private AutoCompleteTextView mEventLocationEt;
     private TextView mEventDateTimeTv;
-    private ProgressDialog mProgressDialog;
     EventDataSource eventDataSource;
-    private LocationManager locationManager;
-    private String provider;
     private static final String TAG_DATETIME_FRAGMENT = "TAG_DATETIME_FRAGMENT";
     private static final String STATE_TEXTVIEW = "STATE_TEXTVIEW";
     private SwitchDateTimeDialogFragment dateTimeFragment;
     private String eventTitle, eventDesc, eventDateTime, eventLocation;
     private RadioButton mCurrentLocationrb, mEnterLocationrb;
     private RadioGroup mRadioGrp;
-    public static final String TAG = CreateEvent.class.getSimpleName();
-    private double mLatitude = 0, mLongitude = 0;
+    private Location mLocation;
+    private static final String LOG_TAG = "PlaceSelectionListener";
+    private static final int REQUEST_SELECT_PLACE = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
-
-
         if (savedInstanceState != null) {
             // Restore value from saved state
             mEventDateTimeTv.setText(savedInstanceState.getCharSequence(STATE_TEXTVIEW));
         }
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar_create_event);
+        setSupportActionBar(mToolbar);
         mToolbar.setTitle("Create Event");
         mToolbar.setNavigationIcon(R.drawable.back);
-
         mEventTitleEt = (EditText) findViewById(R.id.event_title_et);
         mEventTitleEt.requestFocus();
         mEventDescriptionEt = (EditText) findViewById(R.id.event_description_et);
         mEventDateTimeTv = (TextView) findViewById(R.id.event_date_time_et);
         mEventLocationEt = (AutoCompleteTextView) findViewById(R.id.event_location_et);
         mCreateEventBtn = (Button) findViewById(R.id.add_event_btn);
-
         mRadioGrp = (RadioGroup) findViewById(R.id.rdgroup);
+
+        // Method #1
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_fragment);
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+        autocompleteFragment.setHint("Search a Location");
+
+
+        mEventLocationEt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new PlaceAutocomplete.IntentBuilder
+                            (PlaceAutocomplete.MODE_OVERLAY)
+                            .build(CreateEvent.this);
+                    startActivityForResult(intent, REQUEST_SELECT_PLACE);
+                } catch (GooglePlayServicesRepairableException |
+                        GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
         mRadioGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 mEnterLocationrb = (RadioButton) findViewById(R.id.enter_location_rdbtn);
                 mCurrentLocationrb = (RadioButton) findViewById(R.id.current_location_rdbtn);
                 if (mEnterLocationrb.isChecked()) {
-
                     mEventLocationEt.setText("");
 
                 } else if (mCurrentLocationrb.isChecked()) {
-
-                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
+                   /* locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
                     Criteria criteria = new Criteria();
                     provider = locationManager.getBestProvider(criteria, false);
-                    Location location = locationManager.getLastKnownLocation(provider);
-                    if (location != null) {
-                        Log.e("Location ", provider);
-                        onLocationChanged(location);
-                        (new GetUserLocationTask(CreateEvent.this)).execute(location);
+                    Location location = locationManager.getLastKnownLocation(provider);*/
+                    if (mLocation != null) {
+                        (new GetUserLocationTask(CreateEvent.this)).execute(mLocation);
                     } else {
                         mEventLocationEt.setText("Location not available");
-
                     }
-
                 }
-
             }
         });
         eventDataSource = new EventDataSource(this);
-
         mCreateEventBtn.setOnClickListener(new View.OnClickListener()
 
                                            {
@@ -185,18 +203,52 @@ public class CreateEvent extends BaseLocation {
         );
     }
 
+   /* @Override
+    public void onPlaceSelected(Place place) {
+        Log.i(LOG_TAG, "Place Selected: " + place.getName());
+        mEventLocationEt.setText(getString(R.string.formatted_place_data, place
+                .getName(), place.getAddress(), place.getPhoneNumber(), place
+                .getWebsiteUri(), place.getRating(), place.getId()));
+        if (!TextUtils.isEmpty(place.getAttributions())) {
+            attributionsTextView.setText(Html.fromHtml(place.getAttributions().toString()));
+        }
+    }*/
+
+    @Override
+    public void onPlaceSelected(Place place) {
+
+    }
+
+    @Override
+    public void onError(Status status) {
+        Log.e(LOG_TAG, "onError: Status = " + status.toString());
+        Toast.makeText(this, "Place selection failed: " + status.getStatusMessage(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SELECT_PLACE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                this.onPlaceSelected(place);
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                this.onError(status);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     @Override
     public void onLocationChanged(Location location) {
-
+        mLocation = location;
         if (location != null) {
-            mLatitude = location.getLatitude();
-            mLongitude = location.getLongitude();
-            Log.d("Location change", "mlatitude = " + mLatitude + " ,mlongitude = " + mLongitude);
+
+            Log.d("Location change", "mlatitude = " + location.getLatitude() + " ,mlongitude = " + location.getLongitude());
 
         }
 
     }
-
 
     public boolean validateFields() {
         eventTitle = mEventTitleEt.getText().toString();
@@ -295,7 +347,6 @@ public class CreateEvent extends BaseLocation {
         }
     }
 
-
     private class CreateEventTask extends AsyncTask<Void, Void, Boolean> {
         private String eventTitle, eventDesc, eventDateTime, eventLocation;
         private ProgressDialog progressDialog;
@@ -337,6 +388,9 @@ public class CreateEvent extends BaseLocation {
         }
     }
 }
+
+
+
 
 
 
